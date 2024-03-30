97bf74bc4f0f844755f6e43b04797449679d3d30
mohsen@paragrapher <mohsen@paragrapher>
2024-03-30 15:34:26 +0100

    corrections

--- src/webgraph.c
+++ src/webgraph.c
@@ -151,7 +151,14 @@ paragrapher_graph* __wg_open_graph(char* name, paragrapher_graph_type type, void
 	sprintf(graph->underlying_name, "%.*s", PATH_MAX, underlying_name);
 
 	graph->buffer_size = 1024UL * 1024 * 64;
-	graph->max_buffers_count = 2 * get_nprocs();
+	graph->max_buffers_count = get_nprocs();
+	{
+		char res[1024];
+		int ret = (int)__run_command("lscpu | grep \"Thread(s) per core\" | head -n1 | cut -f2 -d:|xargs", res, 1023);
+		assert(ret == 0);
+		unsigned int tpc = atoi(res);
+		graph->max_buffers_count /= tpc; 
+	}
 	graph->offsets = NULL;
 
 	{
